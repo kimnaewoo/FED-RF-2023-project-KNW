5df9b59c7e465cb666f596cf6726e3aba47a1c0d
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-08-30 13:16:25 +0900

    mousepointer

--- 빈폴프로젝트/js/main.js
+++ 빈폴프로젝트/js/main.js
@@ -5,10 +5,19 @@ const qsa = (x) => document.querySelectorAll(x);
 function loadFn() {
     console.log("로딩완료!!");
 
-    const body = qs('body');
-const moving = qs('.flying-poster');
-console.log("추가대상:",body,moving);
-    
-    
-    
+    const body = qs("body");
+    const moving = qs(".flying-poster");
+    console.log("추가대상:", body, moving);
+
+    body.onmousemove = (e) => {
+        console.log("x:", e.screenX, "/y:", e.screenY);
+        moving.style.top = e.screenY + "px";
+        moving.style.left = e.screenX + "px";
+    };
+    body.onmouseenter = () => {
+        moving.style.backgroundImage = `url(${
+            img_src[minfo[0].innerText == "" ? "기본" : minfo[0].innerText]
+        })`;
+        moving.style.display = "block";
+    };
 }
