d20d32803234df57a4787c601d8ceadad33182ea
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-01 15:02:10 +0900

    smooth scroll

--- 빈폴프로젝트/js/main.js
+++ 빈폴프로젝트/js/main.js
@@ -5,7 +5,7 @@ const qsa = (x) => document.querySelectorAll(x);
 
 function loadFn() {
     console.log("로딩완료!!");
-
+     startSS();
     const body = qs("body");
     const moving = qs(".flying-poster");
     console.log("추가대상:", body, moving);
