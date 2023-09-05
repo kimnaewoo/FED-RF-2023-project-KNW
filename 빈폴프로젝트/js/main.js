1095d0235fa615d38fd0b8d10992e4ddc0f0ba51
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-05 22:56:29 +0900

    new brand setup

--- 빈폴프로젝트/js/main.js
+++ 빈폴프로젝트/js/main.js
@@ -11,43 +11,30 @@ const qs = (x) => document.querySelector(x);
 const qsa = (x) => document.querySelectorAll(x);
 
 setTimeout(() => {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 }, 500);
 
 window.addEventListener("DOMContentLoaded", loadFn);
 window.addEventListener("wheel", wheelFn);
 
 function loadFn() {
-    ele_page = qsa("section");
-    console.log('페이지요소:',ele_page);
-    total_pg = ele_page.length;
-    // console.log('페이지요소:',total_pg);
-
-    const moveImg = qsa('span');
-    console.log(moveImg);
-    // for(let x of moveImg){
-    //     x.classList.add("go");
-    // };
-
-    moveImg.forEach((e)=>{
-        console.log(e);
-        e.onclick=(s)=>{
-            console.log("s:",s);
-            e.classList.toggle("go");
-        }
-    });
-
+  ele_page = qsa("section");
+  console.log("페이지요소:", ele_page);
+  total_pg = ele_page.length;
+  // console.log('페이지요소:',total_pg);
 }
 function wheelFn() {
-    if (sts_wheel) return;
-    sts_wheel = 1;
-    setTimeout(() => {sts_wheel = 0;}, 500);
-    console.log("휠이 잘되니?");
-    let delta = event.wheelDelta;
-    console.log("휠델타?", delta);
-    if (delta < 0) pg_num++;
-    else pg_num--;
-    if (pg_num < 0) pg_num = 0;
-    if (pg_num == total_pg) pg_num = total_pg - 1;
-    window.scrollTo(0, window.innerHeight * pg_num);
+  if (sts_wheel) return;
+  sts_wheel = 1;
+  setTimeout(() => {
+    sts_wheel = 0;
+  }, 500);
+  console.log("휠이 잘되니?");
+  let delta = event.wheelDelta;
+  console.log("휠델타?", delta);
+  if (delta < 0) pg_num++;
+  else pg_num--;
+  if (pg_num < 0) pg_num = 0;
+  if (pg_num == total_pg) pg_num = total_pg - 1;
+  window.scrollTo(0, window.innerHeight * pg_num);
 }
