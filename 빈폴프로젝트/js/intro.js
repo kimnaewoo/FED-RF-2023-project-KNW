668a06857e3496b8d361f4e8eead3e955d95f720
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-20 17:51:09 +0900

    main page

--- 빈폴프로젝트/js/intro.js
+++ 빈폴프로젝트/js/intro.js
@@ -40,4 +40,19 @@ domFn.addEvt(span,'mouseout',()=>{
     span.classList.remove('on');
 }); // mouseover 이벤트 함수 
 
+const welcome = domFn.qs('#welcome');
+console.log(welcome);
  
+setInterval(() => {
+    welcome.style = `
+        display: block;
+        transition: 2s;
+        animation: wel-ani 2s forwards ;`
+}, 38500);
+
+setInterval(() => {
+    span.style = `
+        display: block;
+        transition: 2s;
+        animation: en-ani 2s forwards ;`
+}, 39500);
