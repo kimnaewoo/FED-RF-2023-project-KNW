2595d1ed97b2d254d2c9612085cb825e8a2b7bc3
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-21 18:00:31 +0900

    Update main.js

--- 빈폴프로젝트/js/main.js
+++ 빈폴프로젝트/js/main.js
@@ -26,36 +26,7 @@ const domFn = {
    getOT: (ele) => ele.offsetTop,
 }; /////// domFn 객체 /////////////
 
-
-
-// window.addEventListener("DOMContentLoaded", loadFn);
-// window.addEventListener("wheel", wheelFn, { passive: false });
-
-// function loadFn() {
-//   ele_page = domFn.qsa("section");
-//   // // console.log("페이지요소:", ele_page);
-//   total_pg = ele_page.length;
-//   // // console.log('페이지요소:',total_pg);
-
-//   const cbox = domFn.qsa(".cbox_container");
-//   const img = domFn.qsa(".cbox");
-// }
-// function wheelFn() {
-//   event.preventDefault();
-//   if (sts_wheel) return;
-//   sts_wheel = 1;
-//   setTimeout(() => {
-//     sts_wheel = 0;
-//   }, 500);
-//   // // console.log("휠이 잘되니?");
-//   let delta = event.wheelDelta;
-//   // // console.log("휠델타?", delta);
-//   if (delta < 0) pg_num++;
-//   else pg_num--;
-//   if (pg_num < 0) pg_num = 0;
-//   if (pg_num == total_pg) pg_num = total_pg - 1;
-//   window.scrollTo(0, window.innerHeight * pg_num);
-// }
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 const abtn = domFn.qsa(".btn");
 const slider = domFn.qs(".slider");
@@ -105,31 +76,31 @@ abtn.forEach((ele) => {
     }); //////// forEach ///////////
 
     
-    // clearAuto();
+    clearAuto();
   }; ///////// click /////////////
 }); //////////// forEach ////////////
 
-// let autoI, autoT;
+let autoI, autoT;
 
-// function autoSlide() {
-//   autoI = setInterval(() => {
-//     slider.style.left = "-100%";
-//     slider.style.transition = ".6s ease-in-out";
-//     setTimeout(() => {
-//       slider.appendChild(domFn.qsa(".slider>div")[0]);
-//       slider.style.left = "0";
-//       slider.style.transition = "none";
-//     }, 600);
-//   }, 8000);
-// }
+function autoSlide() {
+  autoI = setInterval(() => {
+    slider.style.left = "-100%";
+    slider.style.transition = ".6s ease-in-out";
+    setTimeout(() => {
+      slider.appendChild(domFn.qsa(".slider>div")[0]);
+      slider.style.left = "0";
+      slider.style.transition = "none";
+    }, 600);
+  }, 8000);
+}
 
-// autoSlide();
+autoSlide();
 
-// function clearAuto() {
-//   clearInterval(autoI);
-//   clearTimeout(autoT);
-//   autoT = setTimeout(autoSlide, 3000);
-// }
+function clearAuto() {
+  clearInterval(autoI);
+  clearTimeout(autoT);
+  autoT = setTimeout(autoSlide, 3000);
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////
