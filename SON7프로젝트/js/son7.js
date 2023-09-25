7de4e4a062c70d9982e72ac880bd3aa9f8ca787f
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-25 18:19:26 +0900

    son7 setup

--- SON7프로젝트/js/son7.js
+++ SON7프로젝트/js/son7.js
@@ -3,11 +3,11 @@ import dFn from "./domFn.js"
 const intro = dFn.qs('.intro');
 console.log(dFn,intro);
 
-let hcode = '';
+let hcode = '<div>';
 for(let i=1;i<=19;i++){
     hcode += 
     `
-    <div><img src="./images/camera/${i}.jpg" alt="intro"></div>
+    <img src="./images/intro/camera${i}.jpg" alt="intro">
     `;
 }
 
