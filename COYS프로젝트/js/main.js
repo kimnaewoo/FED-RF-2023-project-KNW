0784676010f562bdf77bb3a3fa61abfee33d2f41
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-12 18:17:36 +0900

    COYS

--- COYS프로젝트/js/main.js
+++ COYS프로젝트/js/main.js
@@ -1,2 +1,10 @@
 import dFn from "./domFn.js"
 
+const start1 = dFn.qs('.start1');
+const start2 = dFn.qs('.start2');
+
+setTimeout(() => {
+    start1.style.transform='translateX(-100%)';
+    start2.style.transform='translateX(100%)';
+}, 2000);
+
