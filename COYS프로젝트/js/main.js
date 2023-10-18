e23de2c1f38fd0dde2b919c45531c6621c49dbe0
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-18 18:20:14 +0900

    ㄴ

--- COYS프로젝트/js/main.js
+++ COYS프로젝트/js/main.js
@@ -3,6 +3,7 @@ import dFn from "./domFn.js"
 const start1 = dFn.qs('.start1');
 const start2 = dFn.qs('.start2');
 const start = dFn.qs('#start')
+const h1 = dFn.qs('#top-area .logo-intro');
 
 setTimeout(() => {
     start1.style.transform='translateX(-100%)';
@@ -11,5 +12,6 @@ setTimeout(() => {
 
 setTimeout(()=>{
     start.style.display="none";
+    h1.style.display='block';
 },5000);
 
