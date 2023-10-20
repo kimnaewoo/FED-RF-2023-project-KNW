f71f1c40049f9860b49efbce02ee2b4d8f103e05
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-20 16:26:12 +0900

    setup

--- COYS프로젝트/js/main.js
+++ COYS프로젝트/js/main.js
@@ -4,6 +4,7 @@ const start1 = dFn.qs('.start1');
 const start2 = dFn.qs('.start2');
 const start = dFn.qs('#start')
 const h1 = dFn.qs('#top-area .logo-intro');
+const vd = dFn.qs('#top-area video');
 
 setTimeout(() => {
     start1.style.transform='translateX(-100%)';
@@ -15,5 +16,10 @@ setTimeout(()=>{
 },4000);
 setTimeout(()=>{
     h1.style.display='block';
+    
 },6000);
+setTimeout(()=>{
+    vd.style.filter= 'brightness(0.7)';
+    
+},6500);
 
