60521b85ec1f568e3bd376999f8e21fbef7f058c
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-31 18:34:57 +0900

    tt PJ

--- COYS프로젝트/js/main.js
+++ COYS프로젝트/js/main.js
@@ -7,12 +7,12 @@ const h1 = dFn.qs('#top-area .logo-intro');
 const vd = dFn.qs('#top-area video');
 
 setTimeout(() => {
-    start1.style.transform='translateX(-100%)';
-    start2.style.transform='translateX(100%)';
+    // start1.style.transform='translateX(-100%)';
+    // start2.style.transform='translateX(100%)';
 }, 2000);
 
 setTimeout(()=>{
-    start.style.display="none";
+    // start.style.display="none";
 },4000);
 setTimeout(()=>{
     h1.style.display='block';
