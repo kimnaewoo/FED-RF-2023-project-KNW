93f365a6b45d56ea12c587a7383b93149725c534
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-11-03 18:19:24 +0900

    tottenham PJ

--- COYS프로젝트/js/main.js
+++ COYS프로젝트/js/main.js
@@ -1,23 +1,22 @@
 import dFn from "./domFn.js"
 
-const start1 = dFn.qs('.start1');
-const start2 = dFn.qs('.start2');
+const msg = dFn.qsa('p');
+const mad = dFn.qs('.mad');
 const start = dFn.qs('#start')
 const h1 = dFn.qs('#top-area .logo-intro');
 const vd = dFn.qs('#top-area video');
 
-setTimeout(() => {
-    // start1.style.transform='translateX(-100%)';
-    // start2.style.transform='translateX(100%)';
-}, 2000);
+
 
 setTimeout(()=>{
-    // start.style.display="none";
-},4000);
+    msg.forEach((ele)=>{
+        ele.style.display='block';
+    })
+},1000);
+
 setTimeout(()=>{
-    h1.style.display='block';
-    
-},6000);
+    mad.classList.add('on');
+},2000);
 setTimeout(()=>{
     vd.style.filter= 'brightness(0.7)';
     
