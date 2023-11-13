1b66f94eb15194b6bd2ba646a9637df82e027fc9
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-11-13 18:18:42 +0900

    COYS PJ

--- COYS프로젝트/js/main.js
+++ COYS프로젝트/js/main.js
@@ -1,6 +1,9 @@
 import dFn from "./domFn.js"
 
 const msg = dFn.qsa('p');
+const mad = dFn.qs('.mad img');
+const son = dFn.qs('.son img');
+const rom = dFn.qs('.rom img');
 const cap = dFn.qsa('.intro-captain');
 const intro = dFn.qs('#intro-area');
 
@@ -10,11 +13,12 @@ setTimeout(()=>{
     })
 },1000);
 setTimeout(()=>{
-    cap.forEach((ele)=>{
-        ele.style.display='block';
-    })
+    rom.style.transform='translate(-40%, -50%)';
 },2000);
 setTimeout(()=>{
-    intro.style.display='none';
-},3000);
+    mad.style.transform='translate(-36%, -50%)';
+},3500);
+setTimeout(()=>{
+    son.style.transform='translate(-45%, -50%)';
+},5000);
 
