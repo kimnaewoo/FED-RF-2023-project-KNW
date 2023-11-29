4d9b28f0215c47bdb62b57961a75dc17beb3eef4
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-11-29 16:09:20 +0900

    Update index.js

--- COYS프로젝트/coys-pj/src/index.js
+++ COYS프로젝트/coys-pj/src/index.js
@@ -30,45 +30,45 @@ function App() {
       $(".top , .bottom").toggleClass("on");
       $(".all-menu").fadeToggle(400);
     }); // click
-  }); // useEffect
-  $(window).scroll(() => {
-    let shopTop = $("#shop-area").offset().top;
-    let ins = $(".is").offset().top;
-    let fk = $(".fk").offset().top;
-    let yt = $(".yt").offset().top;
-    let top = $(".top").offset().top;
-    let bt = $(".bottom").offset().top;
-    let logo = $("#gnb img").offset().top;
-    // console.log(ins, shopTop);
-    if (ins > shopTop) {
-      $(".is").css({ color: "#000" });
-    } else if (ins < shopTop) {
-      $(".is").css({ color: "#fff" });
-    }
+    $(window).scroll(() => {
+      let shopTop = $("#shop-area").offset().top;
+      let ins = $(".is").offset().top;
+      let fk = $(".fk").offset().top;
+      let yt = $(".yt").offset().top;
+      let top = $(".top").offset().top;
+      let bt = $(".bottom").offset().top;
+      let logo = $("#gnb img").offset().top;
+      // console.log(ins, shopTop);
+      if (ins > shopTop) {
+        $(".is").css({ color: "#000" });
+      } else if (ins < shopTop) {
+        $(".is").css({ color: "#fff" });
+      }
 
-    if (fk > shopTop) {
-      $(".fk").css({ color: "#000" });
-    } else if (fk < shopTop) {
-      $(".fk").css({ color: "#fff" });
-    }
+      if (fk > shopTop) {
+        $(".fk").css({ color: "#000" });
+      } else if (fk < shopTop) {
+        $(".fk").css({ color: "#fff" });
+      }
 
-    if (yt > shopTop) {
-      $(".yt").css({ color: "#000" });
-    } else if (yt < shopTop) {
-      $(".yt").css({ color: "#fff" });
-    }
+      if (yt > shopTop) {
+        $(".yt").css({ color: "#000" });
+      } else if (yt < shopTop) {
+        $(".yt").css({ color: "#fff" });
+      }
 
-    if ((top, bt > shopTop)) {
-      $(".top, .bottom").css({ backgroundColor: "#000" });
-    } else if ((top, bt < shopTop)) {
-      $(".top, .bottom").css({ backgroundColor: "#fff" });
-    }
-    if (logo > shopTop) {
-      $("#gnb img").attr("src", "../images/logo.png");
-    } else if (logo < shopTop) {
-      $("#gnb img").attr("src", "../images/logo2.png");
-    }
-  }); // scroll 
+      if ((top, bt > shopTop)) {
+        $(".top, .bottom").css({ backgroundColor: "#000" });
+      } else if ((top, bt < shopTop)) {
+        $(".top, .bottom").css({ backgroundColor: "#fff" });
+      }
+      if (logo > shopTop) {
+        $("#gnb img").attr("src", "../images/logo.png");
+      } else if (logo < shopTop) {
+        $("#gnb img").attr("src", "../images/logo2.png");
+      }
+    }); // scroll
+  }, []); // useEffect
 
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
